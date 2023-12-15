8c2cc1994733e57aa10eba6efe89d3ee5f875fd5
rishi921 <shrishidurga21@gmail.com>
2023-12-15 13:25:23 +0530

    Date Format Changes

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+// const formatter = new Intl.DateTimeFormat("en-US", {
+//   day: "2-digit",
+//   month: "long",
+//   year: "numeric",
+// });
+
 const NewsItem = (props)=> {
   
     let {title, description, imageUrl, newsUrl, author, date, source} = props
@@ -20,8 +26,11 @@ const NewsItem = (props)=> {
           <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <p className="card-text"><small className="text-body-secondary">By {!author?"Unknown": author} on {new Date(date).toLocaleString()}</small></p>
-              <a rel="noreferer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+              <p className="card-text"><small className="text-body-secondary">By {!author?"Unknown": author} on {new Date(date).toLocaleString("en-US", { day: "2-digit", month: "short", year: "numeric", hour: "2-digit", minute: "2-digit" })}</small></p>
+              {/* <p className="card-text"><small className="text-body-secondary">By {!author?"Unknown": author} on {formatter.format(date)}</small></p>               */}
+              <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+              {/* <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a> */}
+
           </div>
         </div>
       </div>
